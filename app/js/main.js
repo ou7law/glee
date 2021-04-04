a9bca601908beaf90a50ac2e81ab0449a0cae4f8
ou7law <allstarspoker@mail.ru>
2021-04-04 20:13:23 +0800

    end index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
 $(function(){
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     autoplay: true,
     fade: true,
   });
+
+  var mixer = mixitup('.products__content');
 });
